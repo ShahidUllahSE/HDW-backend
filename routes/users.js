ea46cac96441ab5f0ee8eb378361ccb192619b1b
ShahidUllahSE <shahidbangash8899@gmail.com>
2024-01-08 17:20:38 +0500

    UserRoute validation schema

--- routes/users.js
+++ routes/users.js
@@ -1,14 +1,42 @@
 const userRoute= require("express").Router();
 
+const Joi = require("joi");
+
+const bcrypt= require('bcrypt')
+
+const user_schema = require('./validation_schemas/user_schema')
+
 userRoute.get("/" , ( req, res )=> {
     res.send(" user list ")
 
 })
 
 
-userRoute.post("/", ( req, res) => {
 
-    res.send(" user added ")
+
+
+
+
+userRoute.post("/", async( req, res) => {
+
+    // res.send(" user added ")
+
+
+
+    const {error} = user_schema(req.body)
+  
+
+
+   if (error){
+
+       res.send(error.details[0].message)
+   }
+   else{
+
+    const salt = await bcrypt.genSalt(10)
+    const hash = await bcrypt.hash( req.body.password , salt )
+    res.status(200).send(hash)
+   }
 })
 
 userRoute.patch("/" , ( req, res ) => {
