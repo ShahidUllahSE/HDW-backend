79230753e96a2b7976f204f5df045e4a3fca21e6
ShahidUllahSE <shahidbangash8899@gmail.com>
2024-01-09 13:39:48 +0500

    mongodb connection and jwt

---
+++ jwt/index.js
@@ -0,0 +1,14 @@
+const express = require ('express');
+
+const PORT = 3001;
+
+const app = express();
+
+
+
+
+
+app.listen( PORT , () => console.log(`running on port ${PORT}`) )
+
+
+
---
+++ newroutes/user.js
@@ -0,0 +1,44 @@
+const router = require("express").Router();
+
+router.get("/register" , ( req , res )=>{
+
+
+
+})
+
+router.post("/register" , ( req , res )=>{
+
+    
+
+})
+
+router.patch("/register" , ( req , res )=>{
+
+    
+
+})
+
+
+router.delete("/register" , ( req , res )=>{
+
+    
+
+})
+
+router.get("/login" , ( req , res ) => {
+
+})
+
+
+router.post("/login" , ( req , res ) => {
+    
+})
+
+
+router.patch("/login" , ( req , res ) => {
+    
+})
+
+router.delete("/login" , ( req , res ) => {
+    
+})
