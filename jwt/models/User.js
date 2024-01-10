f0403e3e6d596c75e14c3f9ae9e58a9b99b8a46e
ShahidUllahSE <shahidbangash8899@gmail.com>
2024-01-10 12:31:10 +0500

    exported model based on schema

---
+++ jwt/models/User.js
@@ -0,0 +1,62 @@
+const mongoose = require("mongoose");
+
+// database validation or backend validation and just done before saving database
+
+const userSchema = new mongoose.Schema({
+
+    name: {
+
+        type: String,
+        min: 3,
+        max: 8,
+        required: true
+    },
+
+    email: {
+
+        type: String,
+        min: 5,
+        max: 15,
+        required: true
+
+    },
+
+    password: {
+
+        type: String,
+        min: 8,
+        max:15,
+        required : true
+
+
+
+    },
+
+
+    adress: {
+
+        type: String,
+        min: 1,
+        max: 10,
+        required: false
+
+
+    },
+
+    date: {
+
+        type: Date,
+        default: Data.now()
+
+    },
+
+
+
+})
+
+
+module.exports = mangoose.model("User", userSchema)
+// made model based on Schema and exported simply this means 
+
+
+
